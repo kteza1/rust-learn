f43b0c5678e1ceaa49c358b2c160c8755ef25a8f
kteza1 <k.teza1@gmail.com>
2016-05-24 01:17:43 +0530

    Sample select and channel tests

--- scalable-chat/src/main.rs
+++ scalable-chat/src/main.rs
@@ -1,59 +1,56 @@
-extern crate mio;
-use mio::*;
-use mio::tcp::*;
+#[macro_use]
+extern crate mioco;
+
 use std::net::SocketAddr;
-struct WebSocketServer;
-
-const SERVER: mio::Token = mio::Token(0);
-
-struct Pong {
-    server: TcpListener,
-    connections: Slab<Connection>,
-}
-
-impl mio::Handler for Pong {
-    type Timeout = ();
-    type Message = ();
-
-    fn ready(&mut self,
-             event_loop: &mut mio::EventLoop<Pong>,
-             token: mio::Token,
-             events: mio::EventSet) {
-
-        match token {
-            SERVER => {
-                assert!(events.is_readable());
-
-                println!("server socket is ready to accept connections");
-
-                match self.server.accept() {
-                    Ok(Some(socket)) => {
-                        println!("accepted a connection. created socket");
-                        event_loop.shutdown();
-                    }
-                    Ok(None) => {
-                        println!("the server socket wasn't actually ready");
-                    }
-                    Err(e) => {
-                        println!("listener.accept() errored: {}", e);
-                        event_loop.shutdown();
-                    }
-                }
-            }
-            _ => panic!("Unknown token received"),
-        }
-
-    }
-}
+use std::io::{Read, Write};
+use mioco::tcp::TcpListener;
+use std::thread;
 
 fn main() {
-    let address = "0.0.0.0:6768".parse::<SocketAddr>().unwrap();
-    let server = TcpListener::bind(&address).unwrap();
 
-    let mut event_loop = mio::EventLoop::new().unwrap();
-    event_loop.register(&server, SERVER, EventSet::readable(), PollOpt::edge())
-              .unwrap();
+    let (mail_send, mail_recv) = mioco::sync::mpsc::channel::<Vec<u8>>();
 
-    println!("running pingpong server");
-    event_loop.run(&mut Pong { server: server });
+    thread::spawn(move || {
+        loop {
+            let v = mail_recv.recv().unwrap();
+            println!("{:?}", v);
+        }
+    });
+
+    mioco::start(move || {
+            let addr = "0.0.0.0:9998".parse::<SocketAddr>().unwrap();
+
+            let listener = TcpListener::bind(&addr).unwrap();
+
+            println!("Starting tcp echo server on {:?}",
+                     listener.local_addr().unwrap());
+
+
+            let mut conn = listener.accept().unwrap();
+
+            mioco::spawn(move || {
+                let mut buf = [0u8; 1024 * 16];
+                let mut timer = mioco::timer::Timer::new();
+
+                loop {
+                    timer.set_timeout(5000);
+                    select!(
+                        r:conn => {
+                            let size = conn.read(&mut buf).unwrap();
+                            if size == 0 {
+                                break;
+                            }
+                            conn.write_all(&mut buf[0..size]).unwrap();
+                            let b = buf.to_vec();
+                            let _ = mail_send.send(b);
+                        },
+                        r:timer => {
+                            conn.shutdown(mioco::tcp::Shutdown::Both).unwrap();
+                        },
+                        );
+                }
+            });
+
+        })
+        .unwrap();
 }
--- scalable-chat/target/debug/scalable_chat.d
+++
@@ -1,5 +0,0 @@
-/Users/ravitejareddy/Desktop/rust-learn/scalable-chat/target/debug/scalable_chat.d: src/main.rs
-
-/Users/ravitejareddy/Desktop/rust-learn/scalable-chat/target/debug/scalable_chat: src/main.rs
-
-src/main.rs:
